9ca487ac1d57f16c484961fcf83d9cb52b640ca6
Bharat Kumar <bharat@>
2021-09-16 11:24:08 +0530

    Route

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from "react-navi";
 import Logo from "../../styles/images/pssroarch-logo-no-icon.png";
 
 export const Header: React.FC = () => {
   return (
     <header>
       <img src={Logo} alt="Proarch" className="logo" />
+      <Link className="profile" href={`/profile`}>Profile</Link>
     </header>
   );
 };
